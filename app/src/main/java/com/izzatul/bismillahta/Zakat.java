afd5c131eb26c795b8d84da9360528777fb1d5ed
IzzatulMillah <izzatulmillah27@gmailcom>
2018-01-24 11:31:51 +0700

    first commit

---
+++ app/src/main/java/com/izzatul/bismillahta/Zakat.java
@@ -0,0 +1,8 @@
+package com.izzatul.bismillahta;
+
+/**
+ * Created by Izzatul on 1/24/2018.
+ */
+
+public class Zakat {
+}
---
+++ app/src/main/java/com/izzatul/bismillahta/ZakatAdapter.java
@@ -0,0 +1,8 @@
+package com.izzatul.bismillahta;
+
+/**
+ * Created by Izzatul on 1/23/2018.
+ */
+
+public class ZakatAdapter {
+}
