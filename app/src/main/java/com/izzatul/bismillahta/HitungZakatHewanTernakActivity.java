978534bd00c534f7b5681def3638d19a49e07155
IzzatulMillah <izzatulmillah27@gmailcom>
2018-04-05 07:28:33 +0700

    Commit 2.1

--- app/src/main/java/com/izzatul/bismillahta/HitungZakatHewanTernakActivity.java
+++ app/src/main/java/com/izzatul/bismillahta/HitungZakatHewanTernakActivity.java
@@ -2,12 +2,47 @@ package com.izzatul.bismillahta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HitungZakatHewanTernakActivity extends AppCompatActivity {
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class HitungZakatHewanTernakActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hitung_zakat_hewan_ternak);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    // tombol click back ke home atau activity sebelumnya
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int idItem = item.getItemId();
+        switch (idItem) {
+            case android.R.id.home :
+                finish();
+                break;
+            default:
+                Toast.makeText(this, "what are you pushing?", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
