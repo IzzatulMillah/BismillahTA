56099de64261cbb6aeaec659bf18721219bdf453
IzzatulMillah <izzatulmillah27@gmailcom>
2018-05-21 12:46:05 +0700

    end point soal dan jawaban sudah jadi satu

--- app/src/main/java/com/izzatul/bismillahta/LatihanActivity.java
+++ app/src/main/java/com/izzatul/bismillahta/LatihanActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,34 +25,50 @@ import org.json.JSONObject;
 import java.util.Random;
 
 public class LatihanActivity extends AppCompatActivity implements View.OnClickListener{
-    private int idSoal;
+    private int idLatihan;
+    private int urutanSoal = 0;
 
     private static final String TAG = LatihanActivity.class.getName();
-    private String url = "http://192.168.43.20/basic/web/services/get-soal/";
-    private String urlJawaban = "http://192.168.43.20/basic/web/services/get-jawaban/";
+    private String url = "http://192.168.43.20/basic/web/services/get-latihan/";
+//    192.168.43.20
 
-    private TextView noUrutSoal, jumlahSoal, textSoal, jawaban1, jawaban2, jawaban3, jawaban4, jawaban5;
+    private TextView noUrutSoal, jumlahSoal, textSoal, btnNext, btnPrev;
+    private RadioButton jawaban1, jawaban2, jawaban3, jawaban4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_latihan);
 
+        setUpToolbar();
+
+        getElements();
+
+        idLatihan = getRandomNumber();
+        urutanSoal = idLatihan;
+        getSoal(idLatihan);
+        getJawaban(idLatihan);
+
+        btnPrev.setOnClickListener(this);
+        btnNext.setOnClickListener(this);
+    }
+
+    void setUpToolbar(){
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.latihan);
         toolbar.setNavigationIcon(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
+    }
 
+    void getElements(){
         noUrutSoal = findViewById(R.id.tv_urutan_soal);
         textSoal = findViewById(R.id.tv_soal);
         jawaban1 = findViewById(R.id.jawaban1);
         jawaban2 = findViewById(R.id.jawaban2);
         jawaban3 = findViewById(R.id.jawaban3);
         jawaban4 = findViewById(R.id.jawaban4);
-        jawaban5 = findViewById(R.id.jawaban5);
-
-        idSoal = getRandomNumber();
-        getSoal(idSoal);
+        btnPrev = findViewById(R.id.btn_prev);
+        btnNext = findViewById(R.id.btn_next);
     }
 
     // tombol click back ke home atau activity sebelumnya
@@ -94,8 +111,16 @@ public class LatihanActivity extends AppCompatActivity implements View.OnClickLi
                     // response will be a json object
                     JSONObject data = response.getJSONObject("data");
                     String soal = data.getString("soal");
+                    String jawab1 = data.getString("jawaban_1");
+                    String jawab2 = data.getString("jawaban_2");
+                    String jawab3 = data.getString("jawaban_3");
+                    String jawab4 = data.getString("jawaban_4");
 
                     textSoal.setText(soal);
+                    jawaban1.setText(jawab1);
+                    jawaban2.setText(jawab2);
+                    jawaban3.setText(jawab3);
+                    jawaban4.setText(jawab4);
                 } catch (JSONException e) {
                     e.printStackTrace();
                     Toast.makeText(getApplicationContext(),
@@ -131,15 +156,10 @@ public class LatihanActivity extends AppCompatActivity implements View.OnClickLi
                 try {
                     // Parsing json object response
                     // response will be a json object
-                    /*
-                    * Untuk mengambil data jawaban, menggunakan URL atau end point yag sama dengan mengambil data soal
-                     * sebelumnya sudah bisa mengambil data soal, jawabannya belum*/
                     JSONObject data = response.getJSONObject("data");
-                    String jawaban1 = data.getString("jawaban");
-                    String jawaban2 = data.getString("jawaban");
-                    String jawaban3 = data.getString("jawaban");
-                    String jawaban4 = data.getString("jawaban");
-                    String jawaban5 = data.getString("jawaban");
+                    String jawaban_benar = data.getString("jawaban_benar");
+
+                    Toast.makeText(LatihanActivity.this, jawaban_benar, Toast.LENGTH_SHORT).show();
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -164,6 +184,13 @@ public class LatihanActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View view) {
-
+        switch (view.getId()){
+            case R.id.btn_prev :
+                Toast.makeText(this, "TOMBOL PREV CLICKED", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_next :
+                Toast.makeText(this, "TOMBOL NEXT CLICKED", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/izzatul/bismillahta/Soal.java
@@ -0,0 +1,74 @@
+package com.izzatul.bismillahta;
+
+/**
+ * Created by Izzatul on 5/21/2018.
+ */
+
+public class Soal {
+    private String soal;
+    private String jawaban1;
+    private String jawaban2;
+    private String jawaban3;
+    private String jawaban4;
+    private String jawabanBenar;
+    private String jawabanUser;
+
+    public Soal() {
+    }
+
+    public String getSoal() {
+        return soal;
+    }
+
+    public void setSoal(String soal) {
+        this.soal = soal;
+    }
+
+    public String getJawaban1() {
+        return jawaban1;
+    }
+
+    public void setJawaban1(String jawaban1) {
+        this.jawaban1 = jawaban1;
+    }
+
+    public String getJawaban2() {
+        return jawaban2;
+    }
+
+    public void setJawaban2(String jawaban2) {
+        this.jawaban2 = jawaban2;
+    }
+
+    public String getJawaban3() {
+        return jawaban3;
+    }
+
+    public void setJawaban3(String jawaban3) {
+        this.jawaban3 = jawaban3;
+    }
+
+    public String getJawaban4() {
+        return jawaban4;
+    }
+
+    public void setJawaban4(String jawaban4) {
+        this.jawaban4 = jawaban4;
+    }
+
+    public String getJawabanBenar() {
+        return jawabanBenar;
+    }
+
+    public void setJawabanBenar(String jawabanBenar) {
+        this.jawabanBenar = jawabanBenar;
+    }
+
+    public String getJawabanUser() {
+        return jawabanUser;
+    }
+
+    public void setJawabanUser(String jawabanUser) {
+        this.jawabanUser = jawabanUser;
+    }
+}
